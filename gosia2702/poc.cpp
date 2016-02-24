5c2bcb163cc024b5c0c2a293b6ae3d3e95db2f4c
gosia2702 <malgorzata.nachyla@student.uw.edu.pl>
2016-02-24 16:19:29 +0100

    Create poc.cpp

---
+++ gosia2702/poc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+std::string name;
+ cout << "Mount Everest - 8850 m n.p.m.";
+ cout << "McKinley - 6194 m n.p.m.";
+ cout << "Mont Blanc - 4810 m n.p.m."<< end;
+ retun(0);
+ }
